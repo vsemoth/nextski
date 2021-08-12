cbacffefe76ec792b7b9e6c5b50a4a490279adad
vsemoth <vsemoth@gmail.com>
2021-08-12 19:36:04 +0200

    Fixed Module source path

--- pages/SkiWinterSportsShop/index.js
+++ pages/SkiWinterSportsShop/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Footer from "./../../components/Footer";
-import styles from '../styles/Home.module.css'
+import styles from './../../styles/Home.module.css'
 
 const Shop = () => {
   return (
